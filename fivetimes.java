20d17c7ca3c22c5ce2003c191baaca78c5555a6b
ManjuMuralidharan <manju1695@gmail.com>
2016-08-10 19:31:21 +0530

    Add files via upload

---
+++ fivetimes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class fivetimes
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the string");
+		String n=s.nextLine();
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(n);
+		}
+	
+}
+}
